d403effba5ff8b9fedae6409e33600994eabdce0
Anna Dang <annadang2001@gmail.com>
2025-10-06 02:45:40 +1100

    Create new interface to represent game history

--- blackjack-game/src/types.ts
+++ blackjack-game/src/types.ts
@@ -2,3 +2,19 @@ export interface Card {
     name: string;
     value: number;
 }
+
+export type GameResult = "win" | "lose" | "push" | null;
+export type ActionType = "hit" | "stand" | null;
+
+export interface GameHistory {
+    id: string;
+    username: string;
+    timestamp: number;
+    bet: number;
+    result: "win" | "lose" | "push";
+    playerTotal: number;
+    dealerTotal: number;
+    playerCards: Card[];
+    dealerCards: Card[];
+    winnings: number;
+}
